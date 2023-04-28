3f7e890b61ecc238195c137fa79b9ce5e3cce4c3
Cesar <cesarforthomme@gmail.com>
2023-04-28 14:40:05 +0200

    first

---
+++ src/solution.test.ts
@@ -0,0 +1,22 @@
+import { Challenge } from './solution';
+import { assert } from 'chai';
+
+function testSolution(input: number, expectedOutput: number) {
+    const actualOutput = Challenge.solution(input);
+    assert.strictEqual(actualOutput, expectedOutput, `Input: ${input}`);
+}
+
+describe('solution', () => {
+    it('should return 0 for input 0', () => {
+      testSolution(0, 0);
+    });
+  
+    it('should return 0 for input 3', () => {
+      testSolution(3, 0);
+    });
+  
+    it('should return 23 for input 10', () => {
+      testSolution(10, 23);
+    });
+    // Add additional test cases as needed
+  });
---
+++ src/solution.ts
@@ -0,0 +1,18 @@
+export class Challenge {
+    static solution(number: number) {
+        let numbers: number[] = [];
+        for(let i = 0; i < number; i++) {
+            if(i % 3 === 0 || i % 5 === 0) {
+                numbers.push(i);
+            }
+        }
+        const sum = numbers.length < 2 ? 0 : numbers.reduce((a, b) => a + b);
+        return sum;
+    }
+
+    static main() {
+        console.log(Challenge.solution(10));
+    }
+}
+
+Challenge.main();
